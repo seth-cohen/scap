516610a4f610a525b0756ecef5feadae3fc2a295
Seth Cohen <scohen@wayfair.com>
2019-03-12 20:19:19 -0400

    Added iterator for CameraPort

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -31,26 +31,6 @@ void MainWindow::enumerateCameras() {
     a << it->name << ": " << it->port;
     ui->listWidget->addItem(a.str().c_str());
   }
-  /*
-  CameraList *list;
-  GPContext *context;
-  gp_list_new(&list);
-  context = gp_context_new();
 
-  int count = gp_camera_autodetect(list, context);
-  const char *name, *value;
-
-  for (int i = 0; i < count; i++) {
-    gp_list_get_name(list, i, &name);
-    gp_list_get_value(list, i, &value);
-
-    std::stringstream a;
-    a << name << ": " << value;
-
-    ui->listWidget->addItem(a.str().c_str());
-  }
-
-  gp_list_free(list);
-  */
   std::cout << "Number of Cameras Detected from class: " << count << std::endl;
 }
